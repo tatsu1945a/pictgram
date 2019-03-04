2fc97f625d6771c38a6f26a3ee32825fe7701dde
tatsuhiroakita <vmak8160@gmail.com>
2019-03-04 22:30:22 +0900

    3rd

--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'topics/new'
+  get 'sessions/new'
   ##get 'users/new'
   ##get 'pages/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -6,4 +8,12 @@ Rails.application.routes.draw do
   get 'pages/help'
 
   resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+
+  resources :users
+  resources :topics
+
 end

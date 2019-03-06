6eaf6a85b26748afaae21b0575fd2dec921ffce0
tatsuhiroakita <vmak8160@gmail.com>
2019-03-06 18:58:42 +0900

    4th

--- pictgram/config/routes.rb
+++ pictgram/config/routes.rb
@@ -16,4 +16,9 @@ Rails.application.routes.draw do
   resources :users
   resources :topics
 
+  get 'favorites/index'
+  post '/favorites', to: 'favorites#create'
+
+  get 'topics/show?id=#', to: 'topics#show'
+
 end
